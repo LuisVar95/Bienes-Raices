45a43178ed9907158a0706dd37261065d0595d9e
Luis Vargas <miguel95dev@gmail.com>
2023-12-30 21:54:30 -0600

    Primer commit

---
+++ seed/usuario.js
@@ -0,0 +1,18 @@
+import bcrypt from 'bcrypt'
+
+const usuarios = [
+    {
+        nombre: 'Juan',
+        email: 'juan@juan.com',
+        confirmado: 1,
+        password: bcrypt.hashSync('password', 10)
+    },
+    {
+        nombre: 'luis',
+        email: 'luis@luis.com',
+        confirmado: 1,
+        password: bcrypt.hashSync('password', 10)
+    }
+]
+
+export default usuarios
